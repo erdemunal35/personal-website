74cf4c7de0f2a34f58ada928fed5ce2d567af038
Erdem Uenal <erdem.uenal@bmg.com>
2023-01-01 23:08:18 +0100

    Added CV, still need to bring the html form into a proper flask request handling

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-from flask import Flask, render_template
-# import os
+from flask import Flask, render_template, send_from_directory, request, flash, redirect, url_for
 
 # project_root = os.path.dirname(__file__)
 # template_path = os.path.join(project_root, 'web_code')
 # app = Flask(__name__, template_folder=template_path, static_folder=template_path)
 
 app = Flask(__name__)
+# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+app.config['UPLOAD_FOLDER'] = "files"
+
+@app.route('/downloads/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    # Returning file from appended path
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7000)
     # app.run()
 
-# gunicorn --workers 1 --threads 1 --bind 0.0.0.0:7000 --reload app:app 
+# gunicorn --workers 1 --threads 1 --bind 0.0.0.0:7000 --reload app:app  
